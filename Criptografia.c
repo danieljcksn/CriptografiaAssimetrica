1554b3773dc155fd9b1d58254d4ebbeec8949d3b
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-01-24 21:16:24 +0000

    Erro do 'a.txt' solucionado.

--- Criptografia.c
+++ Criptografia.c
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool verificaPrimo(int p1){
 	int i;
@@ -47,10 +48,10 @@ int coPrimo(int phi){
 }
 
 int main(void){
-	int p1, p2, n, phi, e, i, contador;
+	int p1, p2, n, phi, e, i, contador, iteracoes = 0;
 	char linha[50], c, linha2[50];
 	FILE *arquivo, *arq;
-	/*
+
 	printf("Você precisa realizar a inserção de dois números primos:\n");
 	
 	printf("Insira o primeiro número primo!\n");
@@ -75,37 +76,53 @@ int main(void){
 		if(verificaPrimo(p2))
 			break;
 	}
-	*/
+
+	//Limpando a tela
+	printf("\e[H\e[2J");
+
+
 	arquivo = fopen("conjunto.txt", "r");
 	while(fgets(linha, sizeof(linha), arquivo) != NULL){
+		iteracoes++;
 		contador = 0;
 		//Verifica o tamanho da string antes da quebra de linha (\n)
 		for(i = 0; i < sizeof(linha); i++){
-			if(linha[i] == 10)
-				break;
 			contador++;
+			if(linha[i] == 10){
+				contador -= 1;
+				break;
+			}
 		}
 
 		//Criando uma string com o exato tamanho do endereço do arquivo
 		char line[contador];
 
-		//Copiando para este o endereço do arquivo em questão
+		//Copia o endereço para uma variável com tamanho adequado.
 		for(i = 0; i < contador; i++)
 			line[i] = linha[i];
 		
-		printf("%s", line);
+		/*Para solucionar um problema com o armazenamento do primeiro endereço (a.txt), o qual
+		trazia anteriormente consigo lixo da memória na última posição da string.*/
+		if(iteracoes == 1){
+		//caso seja a primeira iteração, ou seja, primeiro endereço lido
+			line[i] = '\0';
+			//atribui nulo ao último caractere, eliminando-o.
+		}
+
 		arq = fopen(line, "r");
-		//if(arq == NULL){
-		//	printf("Erro\n");
-		//}
+		
+		//Verificando se o arquivo foi aberto corretamente.
+		if(arq == NULL)
+			printf("Erro na abertura de arquivo.\n");
+
 
 	}
 
 	fclose(arquivo);
 
 
-	//n = p1 * p2;
-	//phi = (p1-1) * (p2-1);	
-	//e = coPrimo(phi);
+	n = p1 * p2;
+	phi = (p1-1) * (p2-1);	
+	e = coPrimo(phi);
 	return 0;
 }
